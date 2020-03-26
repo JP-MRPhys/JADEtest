6bb118b72f88f44bfef7022c08f8317229ab736f
Jehill Parikh <jehillparikh@gmail.com>
2020-03-26 19:28:46 +0000

    minor updates

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 # Creates a graph.
 c = []
-for d in ['/gpu:1']:
+for d in ['/gpu:0']:
   with tf.device(d):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
