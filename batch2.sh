7739331b6b84c047cae43afc1fe5e3a66b6a8338
Jehill Parikh <jehillparikh@gmail.com>
2020-04-02 14:03:59 +0100

    adding tensorflow files

---
+++ batch2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set name of job
+#SBATCH -J JDPtest1
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+#Select a partition
+#SBATCH --partition=devel
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=jehill.parikh@newcastle.ac.uk
+
+module load cuda/9.0
+module load python3/anaconda
+
+source activate tensorflow-gpu
+python tftest.py
