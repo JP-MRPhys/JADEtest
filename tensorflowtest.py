7eed3a56514c822e4159fa82b2f5aa8f9567f696
Jehill Parikh <jehillparikh@gmail.com>
2020-04-02 22:19:22 +0100

    Update tensorflowtest.py

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -2,7 +2,7 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 # Creates a graph.
 c = []
-for d in ['/gpu:0']:
+for d in ['/gpu:2','/gpu:3' ]:
   with tf.device(d):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
