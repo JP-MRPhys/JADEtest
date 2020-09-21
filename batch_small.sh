a2f52ae3d04b07c0e55dc6a093db09be5f1f121a
jehill <jehillparikh@gmail.com>
2020-09-21 11:57:41 +0100

    refractor

---
+++ batch_small.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set name of job
+#SBATCH -J JDPtest1
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+#Select a partition
+#SBATCH --partition=devel
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=jehill.parikh@newcastle.ac.uk
+
+module load python3/anaconda
+
+source activate tensorflow2-gpu
+python tftest.py
